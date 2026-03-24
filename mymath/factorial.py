d54ecf64e8fcd0e8c7452d45eddac16e67773abf
Yoshiki Cook <yoshiki.cook@outlook.com>
2026-03-24 10:16:48 +0000

    Add precondition

--- mymath/factorial.py
+++ mymath/factorial.py
@@ -1,11 +1,18 @@
-def factorial(n):
+def factorial(n: int) -> int:
     """
     Calculate the factorial of a given number.
 
     :param int n: The factorial to calculate
     :return: The resultant factorial
     """
-    factorial = 1
-    for i in range(1, n + 1):
-        factorial = factorial * i
+    if n < 0:
+        raise ValueError(
+            f"Invalid n: n must be a non-negative integer. "
+            f"Got {n=}"
+        )
+
+    factorial: int = 1
+    for idx in range(1, n + 1):
+        factorial = factorial * idx
+        
     return factorial
