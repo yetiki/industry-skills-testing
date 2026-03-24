ff7689a1a3fce6b9cef1e57d4a6268248f3ce0cf
Yoshiki Cook <yoshiki.cook@outlook.com>
2026-03-24 10:16:27 +0000

    Add tests 0, 5, negative, float

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -2,5 +2,19 @@ import pytest
 from mymath.factorial import factorial
 
 
+def test_0():
+    assert factorial(0) == 1
+
 def test_3():
     assert factorial(3) == 6
+
+def test_5():
+    assert factorial(5) == 120
+
+def test_negative():
+    with pytest.raises(ValueError):
+        factorial(-1)
+
+def test_float():
+    with pytest.raises(TypeError):
+        factorial(0.5)
